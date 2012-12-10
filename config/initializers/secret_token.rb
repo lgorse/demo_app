daff685e22276d030b91c0d861a8261c2e3d73ef
lgorse <lgorse@stanord.edu>
2012-12-10 14:09:00 -0800

    Postgresql dbase

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '3c414c47557e8f6e653d1ebb73ed4f65b00f3f01b14a33aa14404ca6ca5653c1da3f74ab2dfbf3fdaf3e74e7e1da00a536b814fa8edf61100dd9d84db249562e'
+DemoApp::Application.config.secret_token = 'c708372ab01a8a8005e55d1d8c8224e73b8c3c801a6704f52444db03f1d36f34552d15f9c1d1b1f127727d2cb02caae53cafbd00586569236cca78ec05d4a9ce'
